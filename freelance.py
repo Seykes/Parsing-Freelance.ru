a75aca42f367a88a8776ead6ad378fe8e5848acf
Seykes <alakor99@mail.ru>
2021-01-27 20:24:07 +0300

    'UsersListFinally'

--- freelance.py
+++ freelance.py
@@ -17,41 +17,50 @@ def get_links(url, host):
 	return links
 
 
-links = get_links(URL, HOST)
 
-
-def get_pages_count(links):
-	for link in links[2:3]:
-		html = requests.get(link)
-		soup = BeautifulSoup(html.text, 'html.parser')
-		foo = soup.find('ul', class_='pagination pagination-lg')
-		bar = []
-		try:
-			for a in foo.find_all('a', href=True):
-				try:
-					foobar = int(a.string)
-					bar.append(foobar)
-				except:
-					continue
-		except:
-			continue
+def get_pages_count(html):
+	soup = BeautifulSoup(html.text, 'html.parser')
+	foo = soup.find('ul', class_='pagination pagination-lg')
+	bar = []
+	if foo:
+		for a in foo.find_all('a', href=True):
+			try:
+				foobar = int(a.string)
+				bar.append(foobar)
+			except:
+				continue
 		try:
 			return bar[-1]
 		except:
 			return 1
+	else:
+		return 1
 
-
-pages_count = get_pages_count(links)
-print(pages_count)
 
 def get_users(links):
+	foo = 0
+	users_links = []
 	for link in links:
 		html = requests.get(link)
-		users = BeautifulSoup(html.text, 'html.parser').find_all('a', class_='user-card-name')
-		users_links = []
-		for user in users:
-			users_links.append(user.get('href'))
-		return users_links
+		if html.status_code == 200:
+			pages_count = get_pages_count(html)
+			foo += 1
+			print(f'Парсинг раздела {foo} из {len(links)}... ')
+			for page in range(1, pages_count + 1):
+				print(f'Парсинг страницы {page} из {pages_count}...')
+				html = requests.get(link, params={'page': page})
+				users = BeautifulSoup(html.text, 'html.parser').find_all('a', class_='user-card-name')
+				for user in users:
+					users_links.append(user.get('href'))
+		else:
+			print('Подключение не установлено')
+		print(users_links)
+	return users_links
 
 
-users_links = get_users(links)
+def parse():
+	links = get_links(URL, HOST)
+	users = get_users(links)
+
+
+parse()
