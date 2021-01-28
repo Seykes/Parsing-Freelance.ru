cc947727607851e94b616027cf526e6446d0496b
Seykes <alakor99@mail.ru>
2021-01-28 19:10:00 +0300

    'FinallyParisng'

--- freelance.py
+++ freelance.py
@@ -2,7 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-
+import csv
 
 PATH_ROOT = os.path.dirname(__file__)
 URL = 'https://freelance.ru/freelancers/it-i-programmirovanie'
@@ -49,7 +49,6 @@ def get_users(links):
 			foo += 1
 			print(f'Парсинг раздела {foo} из {len(links)}... ')
 			for page in range(1, pages_count + 1):
-				print(f'Парсинг страницы {page} из {pages_count}...')
 				html = requests.get(link, params={'page': page})
 				users = BeautifulSoup(html.text, 'html.parser').find_all('a', class_='user-card-name')
 				for user in users:
@@ -57,33 +56,88 @@ def get_users(links):
 		else:
 			print('Подключение не установлено, get_users.error')
 		print(users_links)
-		return users_links
+	return users_links
 
 
-def get_info(users):
+def get_info(users, host):
 	data = []
 	for user in users:
 		html = requests.get(user)
 		if html.status_code == 200:
 			soup = BeautifulSoup(html.text, 'html.parser')
+			foo = []
+			f = []
+			z = []
+			x = []
+			y = []
+			v = []
+			bar = []
+			foobar = []
+			dialog = []
 			try:
-				data.append({
-						'phone': soup.find('p', class_='phone').get_text()})
+				phone = soup.find('p', class_='phone')
+				for text in phone:
+					f.append(text.text)
 			except: pass
 			try:
-				data.append({
-						'telegram': soup.find('p', class_='telegram').get_text()})
+				telegram = soup.find('p', class_='telegram')
+				for text in telegram:
+					foo.append(text.text)
 			except: pass
+			try:
+				instagram = soup.find('p', class_='instagram')
+				for text in instagram:
+					z.append(text.text)
+			except: pass
+			try:
+				vkontakte = soup.find('p', class_='vkontakte')
+				for text in vkontakte:
+					x.append(text.text)
+			except: pass
+			try:
+				whatsapp = soup.find('p', class_='whatsapp')
+				for text in whatsapp:
+					y.append(text.text)
+			except: pass
+			try:
+				skype = soup.find('p', class_='skype')
+				for text in skype:
+					v.append(text.text)
+			except: pass
+			try:
+				specs = soup.find('div', class_='specs')
+				for text in specs:
+					bar.append(text.text)
+			except: pass
+			try:
+				reviews = soup.find('b', class_='cnt')
+				foobar.append(reviews.text)
+			except: pass
+			try:
+				dialog.append(host + soup.find('a', class_='g_button btn btn-block btn-default send_mess_btn').get('href'))
+			except: pass
+			data.append({'phone': f, 'telegram': foo, 'link': user, 'specs': bar, 'reviews': foobar, 'whatsapp': y,
+						 'vkontakte': x, 'instagram': z, 'dialog': dialog, 'skype': v})
 		else:
 			print('Подключение не установлено, get_info.error')
 	return data
 
 
+def save(data):
+	with open('freelance.csv', 'w', newline='') as file:
+		writer = csv.writer(file, delimiter=';')
+		writer.writerow(['Отзывы', 'Ссылка', 'Диалог', 'Телеграмм', 'Телефон', 'Whatsapp', 'ВК', 'Инстаграмм', 'Skype', 'Навыки'])
+		for item in data:
+			writer.writerow([item['reviews'], item['link'], item['dialog'], item['telegram'], item['phone'],
+							 item['whatsapp'], item['vkontakte'], item['instagram'], item['skype'],item['specs']])
+
+
+
 def main():
 	links = get_links(URL, HOST)
 	users = get_users(links)
-	data = get_info(users)
-	print(data)
+	data = get_info(users, HOST)
+	save(data)
 
 
 if __name__ == '__main__':
